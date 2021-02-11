4d5ee65850499300c4de31d96d5454598802db49
shotius <sh.archemashvili@gmail.com>
2021-02-11 08:49:13 +0400

    6.13

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect }from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import FilterForm from './components/FilterForm'
+import anecdoteServices from './services/anecdotes'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteServices
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  })
+
   return (
     <div>
       <h2>Anecdotes</h2>
       <FilterForm />
-      <Notification />
       <AnecdoteList />
       <AnecdoteForm />
+      <Notification />
     </div>
   )
 }
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,26 +1,26 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+// const anecdotesAtStart = [
+//   'If it hurts, do it more often',
+//   'Adding manpower to a late software project makes it later!',
+//   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+//   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+//   'Premature optimization is the root of all evil.',
+//   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+// ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
+// const asObject = (anecdote) => {
+//   return {
+//     content: anecdote,
+//     id: getId(),
+//     votes: 0
+//   }
+// }
 
-const initialState = anecdotesAtStart.map(asObject)
+// const initialState = anecdotesAtStart.map(asObject)
 
-
-const reducer = (state = initialState, action) => {
+// main reducer
+const reducer = (state = [], action) => {
   // console.log('state now: ', state)
   // console.log('action', action)
   switch(action.type) {
@@ -32,11 +32,14 @@ const reducer = (state = initialState, action) => {
         )
     case 'NEW_ANECDOTE':
       return state.concat(action.data)
+    case 'INIT_ANECDOTES':
+      return action.anecdotes
     default: 
       return state
   }
 }
 
+// action creator
 export const createAnecdote = (content) => {
   return (
     {
@@ -50,11 +53,19 @@ export const createAnecdote = (content) => {
   )
 }
 
+// action creator
 export const voteTo = (id) => {
   return ({
     type: 'VOTE',
     data: {id}
   })
+}
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    anecdotes
+  }
 }
 
 export default reducer
